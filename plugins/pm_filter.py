fe281838cb988f8b7a7313bb29d9ce3f0aad3962
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-12-01 20:32:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -207,7 +207,7 @@ async def next_page(bot, query):
             ]
             for file in files
         ]
-        btn.insert(
+        btn.insert(0,
             [InlineKeyboardButton("ROCKER'S OFFICIAL ©️", url="https://t.me/+Rlq2sxuIJuZhMzM1"),
              InlineKeyboardButton("🖥 𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐎𝐓𝐓 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 UPDATES 🖥", url="https://t.me/+rz0oJ4yZ04ViZmY1")])
             
