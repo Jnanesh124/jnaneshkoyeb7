9f12d9c69a70b07fcb5bb8e271062ae2a0eb0d41
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-12-02 00:05:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -828,8 +828,9 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text=f"🗓 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
              InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")]
+             
         )
-                                     
+    else:                           
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
              )
