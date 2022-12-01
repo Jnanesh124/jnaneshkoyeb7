842b8e99bcd6ee6d2a611de930dff6baf05fc9fe
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-12-01 23:51:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -833,7 +833,6 @@ async def auto_filter(client, msg, spoll=False):
                InlineKeyboardButton("ROCKER'S OFFICIAL ©️", url=f"https://t.me/+Rlq2sxuIJuZhMzM1"), 
                InlineKeyboardButton("🖥 𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐎𝐓𝐓 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 UPDATES 🖥", url=f"https://t.me/+rz0oJ4yZ04ViZmY1") 
               ])
-    else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
              )
