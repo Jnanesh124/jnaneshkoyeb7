dd8c8ae4809fb09fb26be22f5861871fdb70b2c7
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-12-01 20:39:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -209,8 +209,8 @@ async def next_page(bot, query):
         ]
         btn.insert(0,
             [InlineKeyboardButton("ROCKER'S OFFICIAL ©️", url="https://t.me/+Rlq2sxuIJuZhMzM1"),
-             InlineKeyboardButton("🖥 𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐎𝐓𝐓 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 UPDATES 🖥", url="https://t.me/+rz0oJ4yZ04ViZmY1")])
-            
+             InlineKeyboardButton("🖥 𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐎𝐓𝐓 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 UPDATES 🖥", url="https://t.me/+rz0oJ4yZ04ViZmY1")]
+        )  
     else:
         btn = [
             [
@@ -800,10 +800,10 @@ async def auto_filter(client, msg, spoll=False):
             ]
             for file in files
         ]
-            btn.append(
-                [InlineKeyboardButton("ROCKER'S OFFICIAL ©️", url="https://t.me/+Rlq2sxuIJuZhMzM1"),
-                InlineKeyboardButton("🖥 𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐎𝐓𝐓 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 UPDATES 🖥", url="https://t.me/+rz0oJ4yZ04ViZmY1")])
-                
+            btn.insert(0, 
+                [InlineKeyboardButton("ROCKER'S OFFICIAL ©️", url="https://t.me/+Rlq2sxuIJuZhMzM1"), 
+                 InlineKeyboardButton("🖥 𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐎𝐓𝐓 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 UPDATES 🖥", url="https://t.me/+rz0oJ4yZ04ViZmY1")]
+            )                             
     else:
         btn = [
             [
