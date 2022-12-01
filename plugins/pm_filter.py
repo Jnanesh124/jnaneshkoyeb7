f5d2273be56c070c874427592bbb09c9520e01b6
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-12-01 22:57:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -820,12 +820,7 @@ async def auto_filter(client, msg, spoll=False):
             ]
             for file in files
         ]
-
-   btn.insert([
-               InlineKeyboardButton("ROCKER'S OFFICIAL ©️", url=f"https://t.me/+Rlq2sxuIJuZhMzM1"),
-               InlineKeyboardButton("🖥 𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐎𝐓𝐓 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 UPDATES 🖥", url=f"https://t.me/+rz0oJ4yZ04ViZmY1")    
-             ])
-         
+                    
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
         BUTTONS[key] = search
