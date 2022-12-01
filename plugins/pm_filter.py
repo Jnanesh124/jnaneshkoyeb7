e6c87e08182038f9962a1548cb9d91aa9a85d01c
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-12-01 20:15:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -800,7 +800,7 @@ async def auto_filter(client, msg, spoll=False):
             ]
             for file in files
         ]
-            btn.insert(
+            btn.insert(0,
                 [InlineKeyboardButton("ROCKER'S OFFICIAL ©️", url="https://t.me/+Rlq2sxuIJuZhMzM1"),
                 InlineKeyboardButton("🖥 𝐑𝐨𝐜𝐤𝐞𝐫'𝐬 𝐎𝐓𝐓 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 UPDATES 🖥", url="https://t.me/+rz0oJ4yZ04ViZmY1")])
                 
